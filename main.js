58e87e5b7cf8fde359a803a429db62c4735fd7db
Mike Mathew <m2mathew@me.com>
2016-04-22 12:06:30 -0500

    Error message appears

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var wordBank = ['the', 'bank', 'is', 'open', 'every', 'single', 'day', 'dude'];
+var wordBank = ['the', 'bank', 'is', 'open', 'every', 'single', 'day', 'dude', 'man', 'person', 'dog', 'turtle'];
 
 function randomWord() {
 	var word = wordBank[Math.floor(Math.random() * wordBank.length)];
@@ -6,15 +6,36 @@ function randomWord() {
 	return word;
 }
 
+function messageDisplay() {
+	message.firstChild.nodeValue = 'Let\'s do this';
+}
+
 var startButton = document.getElementById('start-button');
 var message = document.getElementById('message');
+var inputForm = document.getElementById('form');
+var errorMessage = document.getElementById('error-message');
 
 startButton.addEventListener('click', function() {
-
-	message.firstChild.nodeValue = 'Let\'s do this';
-
+	messageDisplay();
 	randomWord();
+
+	startButton.style.display = 'none';
+	inputForm.style.display = 'block';
 
 }, false);
 
+inputForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+	var attempt = e.target[0].value;
 
+	if (attempt === '') {
+		errorMessage.style.display = 'block';
+		errorMessage.textContent = 'Please enter a letter';
+	}
+
+	if (attempt.length !== 1) {
+		errorMessage.style.display = 'block';
+		errorMessage.textContent = 'Please enter only one letter';
+	}
+
+}, false);
