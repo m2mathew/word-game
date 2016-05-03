5221ce90bdece7925e3ef83b11d9c33ac5e95576
Mike Mathew <m2mathew@me.com>
2016-05-02 22:45:17 -0500

    Begin checking guess

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
+// Initialize variables
 var wordBank = ['the', 'bank', 'is', 'open', 'every', 'single', 'day', 'dude', 'man', 'person', 'dog', 'turtle', 'drum', 'internet', 'game'];
 var wordLength = 0;
+var pickedWord = '';
 var guessWord = '';
 var counter = 0;
+var startButton = document.getElementById('start-button');
+var message = document.getElementById('message');
+var inputForm = document.getElementById('form');
+var errorMessage = document.getElementById('error-message');
+var wordSection = document.getElementById('word-section');
+var wordDisplay = document.getElementById('word-display');
+var input = document.getElementById('letter-submit');
 
+// Initialize functions
 function randomWord() {
-	var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-	wordLength = word.length;
+	pickedWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	wordLength = pickedWord.length;
 	console.log('word length:', wordLength);
-	console.log('the chosen word is:', word);
-	if (word.length < 3) randomWord();
-	blankWord();
-	return word;
+	console.log('the chosen word is:', pickedWord);
+	if (pickedWord.length < 3) randomWord();
+	guessedWord();
+	return pickedWord;
 }
 
 function messageDisplay() {
 	message.firstChild.nodeValue = 'Let\'s do this';
 }
 
-function blankWord() {
+function guessedWord() {
 	var newArray = [];
 	for(var i = 0; i < wordLength; i++) {
 		newArray.push('_ ');
@@ -32,14 +42,13 @@ function checkForLetter(letter) {
 	return letter.length === 1 && letter >= 'a' && letter <= 'z';
 }
 
-var startButton = document.getElementById('start-button');
-var message = document.getElementById('message');
-var inputForm = document.getElementById('form');
-var errorMessage = document.getElementById('error-message');
-var wordSection = document.getElementById('word-section');
-var wordDisplay = document.getElementById('word-display');
-var input = document.getElementById('letter-submit');
+function checkGuess(guess) {
+	console.log('let me check that guess:', guess);
+}
 
+// Interact with the DOM
+
+// Start button shows game board
 startButton.addEventListener('click', function() {
 	messageDisplay();
 	randomWord();
@@ -54,6 +63,7 @@ startButton.addEventListener('click', function() {
 
 }, false);
 
+// Display error message on keyUp
 input.addEventListener('input', function(e) {
 	var attempt = e.target.value;
 
@@ -75,8 +85,16 @@ input.addEventListener('input', function(e) {
 
 }, false);
 
+// Handle submit
 inputForm.addEventListener('submit', function(e) {
 	e.preventDefault();
 	var attempt = e.target[0].value;
+
+	if (attempt === '') {
+		errorMessage.textContent = 'Please enter something!';
+	}
+	else {
+		checkGuess(attempt);
+	}
 
 }, false);
